8dbccc7499ccefc0bdf148169d141d970853dffd
Ricardo Geada <142402302+RicardoGeada@users.noreply.github.com>
2025-06-09 13:43:36 +0200

    feat(Hero): add Hero component, add useMediaQuery hook

---
+++ src/components/Hero/Hero.tsx
@@ -0,0 +1,28 @@
+import styles from "./Hero.module.scss";
+import heroImage from "./../../assets/images/tablet/image-hero.png";
+import heroImageLeft from "./../../assets/images/desktop/image-hero-left.png";
+import heroImageRight from "./../../assets/images/desktop/image-hero-right.png";
+import { useMediaQuery } from "../../hooks/useMediaQuery";
+
+function Hero() {
+
+  const isDesktop = useMediaQuery("(min-width: 1080px)")
+    
+  return (
+    <section className={styles["hero-section"]}>
+        
+      <picture>
+        <source media="(min-width: 1080px)" srcSet={heroImageLeft}/>
+        <img src={heroImage} alt="hero-image" />
+      </picture>
+      
+      {isDesktop && (
+        <picture>
+          <img src={heroImageRight} alt="hero-image-right" />
+        </picture>
+      )}
+    </section>
+  );
+}
+
+export default Hero;
