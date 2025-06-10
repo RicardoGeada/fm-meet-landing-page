5d6c1d452579c8b333d73821164cb7d9d902ebe7
Ricardo Geada <142402302+RicardoGeada@users.noreply.github.com>
2025-06-10 23:51:53 +0200

    feat(Hero): add text content to Hero component

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -5,16 +5,26 @@ import heroImageRight from "./../../assets/images/desktop/image-hero-right.png";
 import { useMediaQuery } from "../../hooks/useMediaQuery";
 
 function Hero() {
+  const isDesktop = useMediaQuery("(min-width: 1080px)");
 
-  const isDesktop = useMediaQuery("(min-width: 1080px)")
-    
   return (
     <section className={styles["hero-section"]}>
-        
       <picture>
-        <source media="(min-width: 1080px)" srcSet={heroImageLeft}/>
+        <source media="(min-width: 1080px)" srcSet={heroImageLeft} />
         <img src={heroImage} alt="hero-image" />
       </picture>
+
+      <div className={styles["hero-section__content"]}>
+        <h1 className={styles["hero-section__headline"]}>Group Chat for Everyone</h1>
+        <p className={styles["hero-section__description"]}>
+          Meet makes it easy to connect with others face-to-face virtually and
+          collaborate across any device.
+        </p>
+        <div className={styles["hero-section__buttons"]}>
+          <button>Download v1.3</button>
+          <button>What is it?</button>
+        </div>
+      </div>
 
       {isDesktop && (
         <picture>
