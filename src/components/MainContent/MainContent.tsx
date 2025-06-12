3abc8cefce87ddd4f18bc0b00f8f8d6cfe92380a
Ricardo Geada <142402302+RicardoGeada@users.noreply.github.com>
2025-06-12 14:42:07 +0200

    feat(MainContent): add MainContent component

---
+++ src/components/MainContent/MainContent.tsx
@@ -0,0 +1,13 @@
+import styles from "./MainContent.module.scss";
+
+function MainContent() {
+  return (
+  <section className={styles["main-content"]}>
+    {/* number */}
+    {/* images */}
+    {/* text-content */}
+  </section>
+  );
+}
+
+export default MainContent;
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,11 +1,13 @@
 import Header from "../../components/Header/Header";
 import Hero from "../../components/Hero/Hero";
+import MainContent from "../../components/MainContent/MainContent";
 import "./Home.scss";
 
 function Home() {
   return (<>
     <Header/>
     <Hero/>
+    <MainContent/>
   </>);
 }
 
