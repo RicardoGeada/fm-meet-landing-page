669ed90d9ad9eb7f5c5a91fb122296ec187d510c
Ricardo Geada <142402302+RicardoGeada@users.noreply.github.com>
2025-06-17 09:12:53 +0200

    feat(MainContent): add image collage

--- src/components/MainContent/MainContent.tsx
+++ src/components/MainContent/MainContent.tsx
@@ -1,13 +1,22 @@
 import NumberDivider from "../SectionDivider/SectionDivider";
 import styles from "./MainContent.module.scss";
+import imageWomanInVideocall from "./../../assets/images/desktop/image-woman-in-videocall.jpg";
+import imageWomenVideochatting from "./../../assets/images/desktop/image-women-videochatting.jpg";
+import imageMenInMeeting from "./../../assets/images/desktop/image-men-in-meeting.jpg";
+import imageManTexting from "./../../assets/images/desktop/image-man-texting.jpg";
 
 function MainContent() {
   return (
-  <section className={styles["main-content"]}>
-    <NumberDivider number={1} />
-    {/* images */}
-    {/* text-content */}
-  </section>
+    <section className={styles["main-content"]}>
+      <NumberDivider number={1} />
+      <div className={styles["image-collage"]}>
+        <img src={imageWomanInVideocall} alt="" />
+        <img src={imageWomenVideochatting} alt="" />
+        <img src={imageMenInMeeting} alt="" />
+        <img src={imageManTexting} alt="" />
+      </div>
+      {/* text-content */}
+    </section>
   );
 }
 
