35730b321996dcb679382853320629c6c5f20109
Ricardo Geada <142402302+RicardoGeada@users.noreply.github.com>
2025-06-24 14:51:01 +0200

    feat(gh-pages): add gh-pages and minor adjustments

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import "./App.scss";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createHashRouter, RouterProvider } from "react-router-dom";
 import Home from "./pages/Home/Home";
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
     path: "/",
     element: <Home />,
